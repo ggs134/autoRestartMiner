af6e20c1c635416e07cf7cf8ea9e973f5d665b74
miner12 <miner12@ubuntu.netis>
2016-05-25 18:22:36 +0900

    first commit

---
+++ autoRestart.py
@@ -0,0 +1,26 @@
+import os
+import time
+import sys
+
+com_clock = ""
+path = "/home/miner12/ethminer.err.log"
+
+def restart():
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    #command = "/sbin/shutdown -r now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print output
+
+while 1:
+  with open(path) as f:
+    lines = f.readlines()
+    last = lines[-1]
+    clock = last.split()[1]
+    if clock == com_clock:
+      sys.stdout.write("Something Wrong")
+      restart()
+    print sys.stdout.write(clock + " No Problem ")
+    com_clock = clock
+  time.sleep(300)
